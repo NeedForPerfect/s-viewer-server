bf9464f9d96f4108d96894f1f2d51ae02ceeedb6
unknown <realffootball@gmail.com>
2020-06-01 03:28:44 +0300

    fix home page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  hello(): string {
+    return 'HEllo World';
+  }
+
   @Get('hello')
   getHello(): string {
     return this.appService.getHello();
