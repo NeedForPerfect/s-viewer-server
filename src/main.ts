292ab56f225f19a07839768a672cf29f3d360fe4
unknown <realffootball@gmail.com>
2020-06-03 21:21:45 +0300

    Get DB Password from env Variable

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Client } from 'pg';
+require("dotenv/config");
+
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+const client = new Client({
+  user: 'lraqlqpculjgps',
+  host: 'ec2-176-34-97-213.eu-west-1.compute.amazonaws.com',
+  database: 'd2ia90tbiq7o1h',
+  password: DB_PASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+client.connect();
+console.log('Connects');
+
+client.query('SELECT * FROM public.suplies ORDER BY id ASC', (err, res) => {
+  console.log(err, res.rows)
+  client.end()
+})
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
